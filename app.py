aa3e9c30f3723e7aeb959dd9938b975f5678cd6f
Denys Holovatyi <dh@osnova.services>
2023-11-12 16:58:50 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import openai
+import re
+import streamlit as st
+from prompts import get_system_prompt
+
+openai.api_key = st.secrets.OPENAI_API_KEY
+
+st.title("ProcessGPT - Snowflake & ChatGPT")
+import json
+
+def import_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+# Initialize the chat messages history
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
+
+# Prompt for user input 
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    if message["role"] == "system":
+        continue
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        if "results" in message:
+            st.dataframe(message["results"])
+
+# If last message is not from assistant, we need to generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        response = ""
+        resp_container = st.empty()
+
+        for delta in openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            response += delta.choices[0].delta.get("content", "")
+            resp_container.markdown(response)
+
+        message = {"role": "assistant", "content": response}
+
+        # Parse the response for a SQL query and execute if available
+        sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+
+        if sql_match:
+            sql = sql_match.group(1)
+            conn = st.connection("snowflake")
+            message["results"] = conn.query(sql)
+            st.dataframe(message["results"])
+        
+        st.session_state.messages.append(message)
---
+++ prompts.py
@@ -0,0 +1,145 @@
+import streamlit as st
+
+QUALIFIED_TABLE_NAME_CASES = "PROCESSMINING.PUBLIC.CASES"
+QUALIFIED_TABLE_NAME_ACTIVITIES = "PROCESSMINING.PUBLIC.ACTIVITIES"
+# METADATA_QUERY = "SELECT VARIABLE_NAME, DEFINITION FROM FROSTY_SAMPLE.CYBERSYN_FINANCIAL.FINANCIAL_ENTITY_ATTRIBUTES_LIMITED;"
+TABLE_DESCRIPTION = """
+This table has various metrics for on the order-to-cash process of an e-commerce company.
+"""
+
+GEN_SQL = """
+You will be acting as an AI Snowflake SQL expert named ProcessGPT specialized on process mining.
+
+You are an experienced process mining data analyst. 
+
+You understand to keep concepts and process mining such as activity, timestamp, case ID that constitute an event log. You also understand the concept of a case table that contains documents such as invoices, sales orders, customer service tickets depending on the process.
+
+•    consider the fundamental data model structure: case table containing the main documents and respective case attributes, activity table with activities describing what happens in each case with timestamps and temporal order
+•    consider that an activity can repeat within a case and how it impacts calculations
+•    Decide which data is required to answer the question. Several things are important: can you calculate the answer based only on case data (column names are helpful)? What case attributes do you need (unique values of the columns are helpful)? What activity data do you need?
+•    Plan on the logical steps of action to complete the calculation
+•    answer each of the questions I ask to the best of your ability, be succinct, explain your reasoning
+•    think step by step if necessary
+•    make assumptions and proceed
+•    try to minimize complexity
+
+Your goal is to give correct, executable SQL queries to users.
+
+You will be replying to users who will be confused if you don't respond in the character of ProcessGPT.
+
+You are given two tables 
+
+<tableName>CASES</tableName>
+<columns>"Case ID", REGION, COST, "Account Manager", "Product Group", "Customer Group"</columns>
+
+<tableName>ACTIVITIES</tableName>
+<columns>"Case ID", Activity, "Start Time", SAP_User, Organization</columns>
+<activities>
+Sales Order
+Outbound Delivery
+Handling unit
+Shipment
+Invoice
+Quotation
+Customer pick-up
+Purchase Order to Supplier
+Returns Delivery
+GD ret.del. returns
+Credit Memo
+Delivery Changed
+Payment received
+Return w Notific.
+Confirmation of service
+Sales Order Changed (VA02)
+</activities>
+
+The user will ask questions; for each question, you should respond and include a SQL query based on the question and the table. 
+
+{context}
+
+Here are critical rules for the interaction you must abide:
+<rules>
+1. You MUST wrap the generated SQL queries within ``` sql code markdown in this format e.g
+```sql
+(select 1) union (select 2)
+```
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
+4. Make sure to generate a single Snowflake SQL code snippet, not multiple. 
+5. You should only use the table columns given in <columns>, and the tables given in <tableName>, you MUST NOT hallucinate about the table names.
+6. DO NOT put numerical at the very front of SQL variable.
+7. Most questions require joining these tables. This is the standard join condition:
+SELECT * FROM PROCESSMINING.PUBLIC.CASES AS C 
+    JOIN PROCESSMINING.PUBLIC.ACTIVITIES AS A ON
+        A."Case ID" = C."Case ID"
+8. Make use of <activities> for activity names
+</rules>
+
+Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
+and wrap the generated sql code with ``` sql code markdown in this format e.g:
+```sql
+(select 1) union (select 2)
+```
+
+For each question from the user, make sure to include a query in your response.
+
+Now to get started, please briefly introduce yourself, describe the table at a high level, and share the available metrics in 2-3 sentences.
+Then provide 3 example questions using bullet points, similar to:
+
+1. How big percentage of orders is invoiced?
+2. What product is most often delivered by the supplier?
+3. What are the 3 most common reasons for the order to be returned by customer (“Return w Notific.”)
+
+Avoid writing SQL code in example questions
+"""
+
+# @st.cache_data(show_spinner=False)
+def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")
+    conn = st.connection("snowflake")
+    columns = conn.query(f"""
+        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
+        """,
+                         )
+    columns = "\n".join(
+        [
+            f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
+            for i in range(len(columns["COLUMN_NAME"]))
+        ]
+    )
+    context = f"""
+        Here is the table name <tableName> {'.'.join(table)} </tableName>
+
+        <tableDescription>{table_description}</tableDescription>
+
+        Here are the columns of the {'.'.join(table)}
+
+        <columns>\n\n{columns}\n\n</columns>
+    """
+    if metadata_query:
+        metadata = conn.query(metadata_query)
+        metadata = "\n".join(
+            [
+                f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
+                for i in range(len(metadata["VARIABLE_NAME"]))
+            ]
+        )
+        context = context + \
+            f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
+    return context
+
+
+def get_system_prompt():
+    table_context = get_table_context(
+        table_name=QUALIFIED_TABLE_NAME_CASES,
+        table_description=TABLE_DESCRIPTION,
+        # metadata_query=METADATA_QUERY
+    )
+    return GEN_SQL.format(context=table_context)
+
+
+# do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
+if __name__ == "__main__":
+    st.header("System prompt for ProcessGPT")
+    st.markdown(get_system_prompt())
